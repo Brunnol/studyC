861ee5b4db375c4b4f7d993a327a41ecf8fefec1
Brunno Lukas Moura Teixeira <38947543+Brunnol@users.noreply.github.com>
2019-07-04 21:57:41 -0300

    codigo 100% funcional e pronto pro uri

--- hora.c
+++ hora.c
@@ -1,15 +1,19 @@
-#iclude <stdio.h>
+#include <stdio.h>
 int main(){
-
+  
   int N, H, M, S;
+  //declarar dua variavei inteira
   
-  scanf("%d", &N);
+	scanf("%d", &N);
+
+  //pegar as avariaveis
   
-  H = N / 3600;
-  M = N / 60;
-  S = N / 6;
-  
-  printf("%d:%d:%d", H, M, S);
-  
-  return 0;
+	H = N / 3600 ;
+	M = N % 3600 / 60;
+	S = N % 60;
+	
+ //pegar as avariaveis
+  printf("%d:%d:%d\n", H, M, S);
+
+    return 0;
 }
